626dd53317db9ca4ea89c3dd1b81125eb0476dd9
assafmo <assaf.morami@gmail.com>
2018-06-12 20:19:57 +0300

    test output ordered by time

--- main_test.go
+++ main_test.go
@@ -9,10 +9,65 @@ import (
 	"github.com/google/gopacket/pcapgo"
 )
 
-// TestPacketCount packet count of merged pcap
+func packetCount(pcapPath string) (uint64, error) {
+	inputFile, err := os.Open(pcapPath)
+	if err != nil {
+		return 0, err
+	}
+	defer inputFile.Close()
+
+	reader, err := pcapgo.NewReader(inputFile)
+	if err != nil {
+		return 0, err
+	}
+
+	var packetCount uint64
+	for {
+		_, _, err = reader.ReadPacketData()
+		if err == io.EOF {
+			return packetCount, nil
+		} else if err != nil {
+			return 0, err
+		}
+		packetCount++
+	}
+}
+
+func isTimeOrdered(pcapPath string) (bool, error) {
+	inputFile, err := os.Open(pcapPath)
+	if err != nil {
+		return false, err
+	}
+	defer inputFile.Close()
+
+	reader, err := pcapgo.NewReader(inputFile)
+	if err != nil {
+		return false, err
+	}
+
+	var previousTime int64
+	for {
+		_, capInfo, err := reader.ReadPacketData()
+		if err == io.EOF {
+			return true, nil
+		} else if err != nil {
+			return false, err
+		}
+
+		currentTime := capInfo.Timestamp.UnixNano()
+
+		if currentTime < previousTime {
+			return false, nil
+		}
+
+		previousTime = currentTime
+	}
+}
+
+// TestCount packet count of merged pcap
 // should be the sum of the packet counts of the
 // input pcaps
-func TestPacketCount(t *testing.T) {
+func TestCount(t *testing.T) {
 	outputFile, err := ioutil.TempFile("", "joincap_output_")
 	if err != nil {
 		t.Fatal(err)
@@ -20,48 +75,51 @@ func TestPacketCount(t *testing.T) {
 	outputFile.Close()
 	defer os.Remove(outputFile.Name())
 
-	inputFilePath := "examples/ok.pcap"
+	inputFilePath := "pcap_examples/ok.pcap"
 
 	joincap([]string{"joincap", "-w", outputFile.Name(), inputFilePath, inputFilePath})
 
-	inputCount, err := countPackets(inputFilePath)
+	inputPacketCount, err := packetCount(inputFilePath)
 	if err != nil {
 		t.Fatal(err)
 	}
-	outputCount, err := countPackets(outputFile.Name())
+	outputPacketCount, err := packetCount(outputFile.Name())
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if inputCount*2 != outputCount {
-		t.Fatalf("inputCount*2 != outputCount (%d != %d)\n", inputCount*2, outputCount)
+	if inputPacketCount*2 != outputPacketCount {
+		t.Fatalf("inputPacketCount*2 != outputPacketCount (%d != %d)\n", inputPacketCount*2, outputPacketCount)
 	}
 }
 
-func countPackets(pcapPath string) (packetCount uint64, err error) {
-	inputFile, err := os.Open(pcapPath)
+// TestOrder all packets in merged pacap should
+// be ordered by time
+func TestOrder(t *testing.T) {
+	outputFile, err := ioutil.TempFile("", "joincap_output_")
 	if err != nil {
-		return
+		t.Fatal(err)
 	}
-	defer inputFile.Close()
+	outputFile.Close()
+	defer os.Remove(outputFile.Name())
 
-	reader, err := pcapgo.NewReader(inputFile)
+	inputFilePath := "pcap_examples/ok.pcap"
+
+	joincap([]string{"joincap", "-w", outputFile.Name(), inputFilePath, inputFilePath})
+
+	isInputOrdered, err := isTimeOrdered(inputFilePath)
 	if err != nil {
-		return
+		t.Fatal(err)
+	}
+	if !isInputOrdered {
+		t.Fatalf("inputFile %s is not ordered by time\n", inputFilePath)
 	}
 
-	for {
-		_, _, err = reader.ReadPacketData()
-		if err != nil {
-			if err == io.EOF {
-				err = nil
-				break
-			} else {
-				return
-			}
-		}
-		packetCount++
+	isOutputOrdered, err := isTimeOrdered(outputFile.Name())
+	if err != nil {
+		t.Fatal(err)
 	}
-
-	return
+	if !isOutputOrdered {
+		t.FailNow()
+	}
 }
