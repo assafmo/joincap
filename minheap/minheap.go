b24985b66ca18f653edcb87265af4d0076fa405e
assafmo <assaf.morami@gmail.com>
2018-07-03 02:22:48 +0300

    clarify heap comment

--- minheap/minheap.go
+++ minheap/minheap.go
@@ -7,7 +7,7 @@ import (
 	"github.com/google/gopacket/pcapgo"
 )
 
-// Packet is used by PacketHeap to order packets
+// Packet is used by PacketHeap to order packets by timestamp
 // and by joincap to merge pcaps
 type Packet struct {
 	Timestamp             int64
