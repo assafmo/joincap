632f64a030ba12bf5270c8d106d2dfae4a525788
assafmo <assaf.morami@gmail.com>
2019-05-06 09:32:55 +0300

    DEBIAN/control Installed-Size, Provides, Conflicts, Replaces

--- build.sh
+++ build.sh
@@ -27,20 +27,24 @@ GOOS=darwin  GOARCH=amd64 go build -o "release/joincap-macos64-${VERSION}"
     parallel --bar 'zip "$(echo "{}" | sed "s/.exe//").zip" "{}" && rm -f "{}"'
 
     # deb
-    mkdir -p ./deb/DEBIAN
-    cat > ./deb/DEBIAN/control <<EOF 
-Package: joincap
-Architecture: amd64
-Maintainer: Assaf Morami <assaf.morami@gmail.com>
-Priority: optional
-Version: $(echo "${VERSION}" | tr -d v)
-Homepage: https://github.com/assafmo/joincap
-Description: Merge multiple pcap files together, gracefully.
-EOF
-
     mkdir -p ./deb/bin
     unzip -o -d ./deb/bin joincap-linux64-*.zip
     mv -f ./deb/bin/joincap-linux64-* ./deb/bin/joincap
+
+    mkdir -p ./deb/DEBIAN
+    cat > ./deb/DEBIAN/control <<EOF 
+Package: joincap
+Version: $(echo "${VERSION}" | tr -d v)
+Priority: optional
+Architecture: amd64
+Maintainer: Assaf Morami <assaf.morami@gmail.com>
+Homepage: https://github.com/assafmo/joincap
+Installed-Size: $(stat --printf="%s" ./deb/bin/joincap)
+Provides: joincap
+Conflicts: joincap
+Replaces: joincap
+Description: Merge multiple pcap files together, gracefully.
+EOF
 
     dpkg-deb --build ./deb/ .
 )
